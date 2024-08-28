5e0cc042e53b7812cfb62d7c337f31c25282419c
Shin-jun <gkwk56@gmail.com>
2024-08-28 23:45:41 +0900

    init

---
+++ src/test/java/com/Kooont/HeritageRoad/HeritageRoadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kooont.HeritageRoad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeritageRoadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
